a9e6b31105f9077c7ff8e983016120f978951730
taleshge21gt <124158196+taleshge21gt@users.noreply.github.com>
2024-08-22 17:00:52 -0300

    server js

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
 
-// Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
-
-// Comment out to allow write operations
 const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
@@ -26,5 +16,4 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
