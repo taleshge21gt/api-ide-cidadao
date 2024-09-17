dafeb1cde0bf1f2070827be958c7a29551fc0cee
Tales <tales.habib@grupoge21.com>
2024-09-17 10:55:42 -0300

    create post

--- server.js
+++ server.js
@@ -16,4 +16,32 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
+function createFormData(formId, name, description, createdAt, updatedAt, version, users = [], id) {
+    return {
+      form_id: formId,
+      name: name,
+      description: description,
+      createdAt: createdAt,
+      updatedAt: updatedAt,
+      version: version,
+      users: users,
+      id: id,
+    };
+  }
+  
+  fetch("http://localhost:3000/form", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(createFormData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
 module.exports = server
