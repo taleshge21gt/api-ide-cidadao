4e0fa0ff138254a11ab4dd464d41a7a4c82c1ca8
Tales <tales.habib@grupoge21.com>
2024-09-17 13:37:29 -0300

    update api

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const jsonServer = require('json-server')
 
-const path = require('path')
-
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
@@ -17,4 +25,5 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
+// Export the Server API
 module.exports = server
